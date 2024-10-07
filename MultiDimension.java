50eb70da33e27429b8ecefacb43b17b63ddc6b1b
ikmal3499 <ikmalmohydin@gmail.com>
2024-10-07 12:07:16 +0800

    Multi Dimensional

--- MultiDimension.java
+++ MultiDimension.java
@@ -11,6 +11,7 @@ public class MultiDimension {
         int[][] matrix = {
             {1,2,3},
             {4,5,6,7},
+            {8,9}
         };
 
         System.out.println("Matrix length : " + matrix.length);
